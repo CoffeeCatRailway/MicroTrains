f0db851f8dad7064bb44f8229a352c11f56788ab
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-06-29 20:21:17 +1000

    Added hold shift hor rolling stock & locomotives

--- src/main/java/coffeecatteam/microtrains/objects/items/ItemBase.java
+++ src/main/java/coffeecatteam/microtrains/objects/items/ItemBase.java
@@ -1,7 +1,17 @@
 package coffeecatteam.microtrains.objects.items;
 
 import coffeecatteam.microtrains.MicroTrains;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemBase extends Item {
 
@@ -9,5 +19,15 @@ public class ItemBase extends Item {
         setUnlocalizedName(name);
         setRegistryName(name);
         setCreativeTab(MicroTrains.MICROTAB);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        if (GuiScreen.isShiftKeyDown()) {
+            String info = I18n.format(this.getUnlocalizedName() + ".info");
+            tooltip.addAll(Minecraft.getMinecraft().fontRenderer.listFormattedStringToWidth(info, 150));
+        } else {
+            tooltip.add(TextFormatting.YELLOW + I18n.format("item.show_info", "SHIFT"));
+        }
     }
 }
